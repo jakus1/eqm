6cf689632d9bb6365169bb5ead390e380589be0b
Richard Memory <richardfmemory@gmail.com>
2018-03-22 08:55:05 -0600

    Null out all existing blades

--- resources/views/member/show_simple.blade.php
+++ resources/views/member/show_simple.blade.php
@@ -1,17 +1,1 @@
-@extends('layouts.modal')
 
-@section('title')
-Member Detail
-@stop
-
-@section('header')
-@parent
-	
-@stop
-
-@section('content')
-	<div>
-		<pre><span ng-bind="member | json"></span></pre>
-	</div>
-
-@stop
